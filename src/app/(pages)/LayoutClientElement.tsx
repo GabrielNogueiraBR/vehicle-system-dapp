81e39ced8454c1bbd97600aacfa3bbbb8ebb9007
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-09-05 07:51:31 -0300

    feat: update content layout

--- src/app/(pages)/LayoutClientElement.tsx
+++ src/app/(pages)/LayoutClientElement.tsx
@@ -4,16 +4,19 @@ import React from 'react'
 import Sidebar from '@/components/Sidebar'
 import { useEthers } from '@usedapp/core'
 import { redirect } from 'next/navigation'
+import { Flex } from '@chakra-ui/react'
 
 const LayoutClientElement = ({ children }: { children: React.ReactNode }) => {
   const { account, isLoading } = useEthers()
   if (!account && !isLoading) redirect('/auth')
 
   return (
-    <>
+    <React.Fragment>
       <Sidebar />
-      {children}
-    </>
+      <Flex flex="1" py="4" px="6">
+        {children}
+      </Flex>
+    </React.Fragment>
   )
 }
 
