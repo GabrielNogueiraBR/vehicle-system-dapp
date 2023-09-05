72b76a467959649fca59fcb5e0160da2df1e2a22
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-09-05 13:52:22 -0300

    feat: adding route to search vehicle nfts

---
+++ src/app/api/vehicles/[address]/get.ts
@@ -0,0 +1,33 @@
+import { CONTRACT_ADDRESS } from '@/constants/web3'
+import alchemy from '@/services/alchemy'
+import { OwnedNft } from 'alchemy-sdk'
+import { NextRequest, NextResponse } from 'next/server'
+
+type GetNftsForAddressResponse = { nfts: OwnedNft[] }
+type Params = { params: { address: string } }
+
+export async function GET(request: NextRequest, { params }: Params) {
+  try {
+    console.log(params)
+    const { address } = params
+
+    if (!address) throw new Error('Invalid data')
+
+    const nfts: OwnedNft[] = []
+
+    const nftsIterable = alchemy.nft.getNftsForOwnerIterator(address, {
+      contractAddresses: [CONTRACT_ADDRESS],
+    })
+
+    for await (const nft of nftsIterable) {
+      nfts.push(nft)
+    }
+
+    const response: GetNftsForAddressResponse = { nfts }
+    return NextResponse.json(response, { status: 200 })
+  } catch (e) {
+    console.error(e)
+    if (e instanceof Error) return NextResponse.json({ error: e.message }, { status: 400 })
+    return NextResponse.json(null, { status: 400 })
+  }
+}
