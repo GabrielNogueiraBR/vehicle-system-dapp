9718a0d4ea11f026885c377669cb5c033d5a1c7f
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-09-05 13:25:58 -0300

    feat: adding vehicle page

--- src/app/(pages)/vehicles/page.tsx
+++ src/app/(pages)/vehicles/page.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+'use client'
 
-const page = () => {
-  return <div>page</div>
+import React from 'react'
+import { Button, Center, Heading, Icon, Img } from '@chakra-ui/react'
+import { BiPlus } from 'react-icons/bi'
+
+const Page = () => {
+  return (
+    <Center flexDirection="column" gap="4" w="100%">
+      <Img src="/no-vehicles.svg" w="sm" alt="no vehicles" loading="lazy" aspectRatio={1} />
+      <Heading as="h3">Sem veículos cadastrados</Heading>
+      <Button colorScheme="green" fontSize="lg" leftIcon={<Icon as={BiPlus} fontSize="2xl" />}>
+        Novo veículo
+      </Button>
+    </Center>
+  )
 }
 
-export default page
+export default Page
