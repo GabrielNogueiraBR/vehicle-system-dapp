964579d3ae117b96e045b50a23e14a34f27682f5
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-09-05 08:06:37 -0300

    feat: adding elements to sidebar

--- src/components/Sidebar/SidebarHeader.tsx
+++ src/components/Sidebar/SidebarHeader.tsx
@@ -1,9 +1,22 @@
 'use client'
 
 import React from 'react'
+import { Heading, VStack, Text, Avatar } from '@chakra-ui/react'
+import { useEthers } from '@usedapp/core'
 
 const SidebarHeader = () => {
-  return <div>SidebarHeader</div>
+  const { account } = useEthers()
+
+  const regex = /^(.{7}).*(.{5})$/
+  const fomartedAccount = account!.replace(regex, '$1...$2')
+
+  return (
+    <VStack spacing={6} align="flex-start">
+      <Heading>Logo</Heading>
+      <Avatar alignSelf="center" size="2xl" name="Logo" />
+      <Text alignSelf="center">{fomartedAccount}</Text>
+    </VStack>
+  )
 }
 
 export default SidebarHeader
