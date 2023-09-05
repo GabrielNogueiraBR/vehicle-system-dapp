b3db47d766eea1317d52c5285b9350e455483e29
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-09-05 07:46:18 -0300

    feat: adding platform home

--- src/components/Sidebar/SidebarContent.tsx
+++ src/components/Sidebar/SidebarContent.tsx
@@ -4,14 +4,17 @@ import React from 'react'
 import { VStack } from '@chakra-ui/react'
 import { NavLink } from './NavLink'
 
-import { RiNftFill, RiShakeHandsLine } from 'react-icons/ri'
+import { RiNftFill, RiShakeHandsLine, RiHome2Line } from 'react-icons/ri'
 
 interface SidebarContentProps {
   isExpanded?: boolean
 }
 
 const SidebarContent = ({ isExpanded = true }: SidebarContentProps) => (
-  <VStack spacing={6} align='flex-start'>
+  <VStack spacing={6} align="flex-start">
+    <NavLink href="/home" icon={RiHome2Line}>
+      Início
+    </NavLink>
     <NavLink href="/vehicles" icon={RiNftFill}>
       Meus Veículos
     </NavLink>
