7fe5de75b6ff6f30513b15dc59e3096d88ed5e28
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-09-05 13:52:30 -0300

    feat: add page vehicles

---
+++ src/app/(pages)/vehicles/ClientElement.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import React from 'react'
+import { Button, Center, Heading, Icon, Img } from '@chakra-ui/react'
+import { BiPlus } from 'react-icons/bi'
+
+const ClientElement = () => {
+  return (
+    <Center flexDirection="column" gap="4" w="100%">
+      <Img src="/no-vehicles.svg" w="sm" alt="no vehicles" loading="lazy" aspectRatio={1} />
+      <Heading as="h3">Sem veículos cadastrados</Heading>
+      <Button colorScheme="green" fontSize="lg" leftIcon={<Icon as={BiPlus} fontSize="2xl" />}>
+        Novo veículo
+      </Button>
+    </Center>
+  )
+}
+
+export default ClientElement
--- src/app/(pages)/vehicles/page.tsx
+++ src/app/(pages)/vehicles/page.tsx
@@ -1,19 +1,7 @@
-'use client'
-
-import React from 'react'
-import { Button, Center, Heading, Icon, Img } from '@chakra-ui/react'
-import { BiPlus } from 'react-icons/bi'
+import ClientElement from './ClientElement'
 
 const Page = () => {
-  return (
-    <Center flexDirection="column" gap="4" w="100%">
-      <Img src="/no-vehicles.svg" w="sm" alt="no vehicles" loading="lazy" aspectRatio={1} />
-      <Heading as="h3">Sem veículos cadastrados</Heading>
-      <Button colorScheme="green" fontSize="lg" leftIcon={<Icon as={BiPlus} fontSize="2xl" />}>
-        Novo veículo
-      </Button>
-    </Center>
-  )
+  return <ClientElement />
 }
 
 export default Page
