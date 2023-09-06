42b3e53d80bfc36c49cebce04990e54abbfcd07e
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-09-06 16:45:41 -0300

    feat: adding data table to project

---
+++ src/components/CustomDataTable/index.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import React from 'react'
+import { Icon } from '@chakra-ui/react'
+import DataTable, { TableProps } from 'react-data-table-component'
+import { RiArrowDownSFill } from 'react-icons/ri'
+
+interface Props<T> extends TableProps<T> {}
+
+function CustomDataTable<T>({...rest}: Props<T>) {
+  return (
+    <DataTable
+      pagination
+      sortIcon={<Icon as={RiArrowDownSFill} ml="2" alignSelf="center" />}
+      {...rest}
+    />
+  )
+}
+
+export default CustomDataTable
