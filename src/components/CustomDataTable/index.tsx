e78b9991c66854500c4a48a225c463df1e2f8f5a
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-09-07 07:41:19 -0300

    feat: adding styles to datatable

--- src/components/CustomDataTable/index.tsx
+++ src/components/CustomDataTable/index.tsx
@@ -2,16 +2,55 @@
 
 import React from 'react'
 import { Icon } from '@chakra-ui/react'
-import DataTable, { TableProps } from 'react-data-table-component'
+import DataTable, { TableProps, createTheme } from 'react-data-table-component'
 import { RiArrowDownSFill } from 'react-icons/ri'
+
+createTheme('transparent', {
+  background: {
+    default: 'transparent',
+  },
+})
 
 interface Props<T> extends TableProps<T> {}
 
-function CustomDataTable<T>({...rest}: Props<T>) {
+function CustomDataTable<T>({ ...rest }: Props<T>) {
   return (
     <DataTable
       pagination
       responsive
+      theme="transparent"
+      customStyles={{
+        header: {
+          style: {
+            minHeight: '56px',
+          },
+        },
+        headRow: {
+          style: {
+            borderTopStyle: 'none',
+            borderTopWidth: '1px',
+            fontWeight: 700,
+            fontSize: 'var(--chakra-fontSizes-sm)',
+          },
+        },
+        headCells: {
+          style: {
+            '&:not(:last-of-type)': {
+              borderRightStyle: 'solid',
+              borderRightWidth: '1px',
+            },
+          },
+        },
+        cells: {
+          style: {
+            fontSize: 'var(--chakra-fontSizes-sm)',
+            '&:not(:last-of-type)': {
+              borderRightStyle: 'solid',
+              borderRightWidth: '1px',
+            },
+          },
+        },
+      }}
       sortIcon={<Icon as={RiArrowDownSFill} ml="2" alignSelf="center" />}
       {...rest}
     />
