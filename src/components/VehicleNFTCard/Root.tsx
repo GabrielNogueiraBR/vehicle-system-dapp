9d33e13d3db4fefbb873c085fe100cd266bb3569
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-09-12 18:53:08 -0300

    feat: adding scale animation

--- src/components/VehicleNFTCard/Root.tsx
+++ src/components/VehicleNFTCard/Root.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import { Flex, FlexProps, LinkBox, LinkOverlay } from '@chakra-ui/react'
 import NextLink from 'next/link'
+import FramerMotionBox from '../FramerMotionBox'
 
 interface Props extends FlexProps {
   tokenId: string
@@ -10,21 +11,32 @@ interface Props extends FlexProps {
 
 const Root = ({ tokenId, children, ...rest }: Props) => {
   return (
-    <LinkBox
-      as={Flex}
-      rounded="xl"
-      w="full"
-      bgGradient="linear(to-r, #8B65FE, #4DFCE0)"
-      p="7px"
-      shadow="xl"
-      {...rest}
+    <FramerMotionBox
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
+      // @ts-ignore no problem in operation, although type error appears.
+      transition={{
+        type: 'spring',
+        stiffness: 260,
+        damping: 20,
+      }}
     >
-      <Flex rounded="xl" bg="white" direction="column" justify="center" align="center" p="6">
-        <LinkOverlay as={NextLink} href={`/vehicles/${tokenId}`}>
-          {children}
-        </LinkOverlay>
-      </Flex>
-    </LinkBox>
+      <LinkBox
+        as={Flex}
+        rounded="xl"
+        w="full"
+        bgGradient="linear(to-r, #8B65FE, #4DFCE0)"
+        p="7px"
+        shadow="xl"
+        {...rest}
+      >
+        <Flex rounded="xl" bg="white" direction="column" justify="center" align="center" p="6">
+          <LinkOverlay as={NextLink} href={`/vehicles/${tokenId}`}>
+            {children}
+          </LinkOverlay>
+        </Flex>
+      </LinkBox>
+    </FramerMotionBox>
   )
 }
 
