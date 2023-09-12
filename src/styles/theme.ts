4814be4b1c61e1dd19c5fc472b27bea173445074
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-09-11 21:38:26 -0300

    feat: adding color theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,6 +6,11 @@ const inter = Inter({ subsets: ['latin'] })
 export const theme = extendTheme({
   colors: {
     primary: '#8B65FE',
+    secondary: '#448DEB',
+    dark: '#3C3C3C',
+    'light-green': '#CBFFFA',
+    'dark-green': '#03DCC7',
+    'light-gray': '#7D7D7D',
   },
   fonts: {
     heading: inter.style.fontFamily,
