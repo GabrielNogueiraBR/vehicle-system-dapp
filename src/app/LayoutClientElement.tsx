2f27518d0cdf6be1596240d036606e10d6b85a5b
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-09-13 13:31:17 -0300

    feat: adding limit to width

--- src/app/LayoutClientElement.tsx
+++ src/app/LayoutClientElement.tsx
@@ -5,7 +5,14 @@ import { Flex } from '@chakra-ui/react'
 
 const LayoutClientElement = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Flex w="100%" minH="100vh" maxH="100%" direction={{ base: 'column', md: 'row' }} bg="gray.100">
+    <Flex
+      w="100%"
+      maxW="100vw"
+      minH="100vh"
+      maxH="100%"
+      direction={{ base: 'column', md: 'row' }}
+      bg="gray.100"
+    >
       {children}
     </Flex>
   )
