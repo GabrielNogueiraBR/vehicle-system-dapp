ae82e7337a4222be7fd902950b289136fdb5c785
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-09-15 08:36:51 -0300

    feat: update layout

--- src/app/LayoutClientElement.tsx
+++ src/app/LayoutClientElement.tsx
@@ -5,7 +5,7 @@ import { Flex } from '@chakra-ui/react'
 
 const LayoutClientElement = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Flex w="100vw" h="100vh" direction={{ base: 'column', md: 'row' }} bg="gray.100">
+    <Flex w="100%" h="auto" minH='100vh' maxW='100vw' direction={{ base: 'column', md: 'row' }} overflowY='auto' bg="gray.100">
       {children}
     </Flex>
   )
