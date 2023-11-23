304cf7f297cb42c4689d297135fb54d78fe623b1
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-11-22 21:49:18 -0300

    feat: adding default redirect

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -1,23 +1,5 @@
-'use client'
-
-import { Button, Flex, Heading, Link } from '@chakra-ui/react'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <Flex
-      flex="1"
-      w="100%"
-      h="100%"
-      direction="column"
-      justify="center"
-      align="center"
-      gap="4"
-      paddingTop="4"
-    >
-      <Heading>Entrar na plataforma</Heading>
-      <Link href="/home">
-        <Button colorScheme="green">Entrar</Button>
-      </Link>
-    </Flex>
-  )
+  redirect('/auth')
 }
