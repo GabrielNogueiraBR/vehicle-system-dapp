65ad702ea2175b5d4cc46d4c8bd87ccaa6f91be3
Gabriel Nogueira <gabrielnoog7@gmail.com>
2023-11-22 21:57:55 -0300

    feat: update contract address

--- src/constants/web3.ts
+++ src/constants/web3.ts
@@ -1,4 +1,4 @@
-export const CONTRACT_ADDRESS = '0x1A8a2Db0536568a27E87c0042943a1B29A4a5968'
+export const CONTRACT_ADDRESS = '0x4D7425d3AB5B31cc82802d35FC1DfcE0Ad136381'
 export const BLOCK_EXPLORER = 'https://mumbai.polygonscan.com'
 
 export const ADDRESS_REGEX = /^(.{0,7}).*?(.{0,5})$/
