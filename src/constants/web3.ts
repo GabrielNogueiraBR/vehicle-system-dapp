5c141313366d262ef5d207113842f92e7b4d4e45
GabrielNogueiraBR <gabrielnoog7@gmail.com>
2024-09-23 18:42:11 -0300

    feat: update contract address

--- src/constants/web3.ts
+++ src/constants/web3.ts
@@ -1,4 +1,4 @@
-export const CONTRACT_ADDRESS = '0xcDFdD14155b6a7dD395dcc3246cE9B4814B6BFb4'
+export const CONTRACT_ADDRESS = '0x5a18Ff1A15c03de8a19A8B21e154Fead4C525393'
 export const BLOCK_EXPLORER = 'https://polygonscan.com/'
 
 export const ADDRESS_REGEX = /^(.{0,7}).*?(.{0,5})$/
